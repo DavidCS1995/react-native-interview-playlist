e184be72245f3326ec9f187d120bff71de8bf8a7
DavidCS1995 <david.c.segovia@gmail.com>
2021-12-30 17:42:21 -0600

    add services

---
+++ src/api/music.service.js
@@ -0,0 +1,22 @@
+import Axios from 'axios';
+
+class MusicService {
+  list = async () => {
+    const url = `https://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=spain&api_key=be8c6a4c47c8e04774909a893e4c64fe&format=json`;
+    console.log(url);
+    return await Axios({
+      method: 'get',
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(json => json.data)
+      .then(response => response.tracks.track)
+      .catch(msg => {
+        console.log(msg);
+      });
+  };
+}
+
+export default new MusicService();
--- src/screens/play/play.screen.js
+++ src/screens/play/play.screen.js
@@ -1,12 +1,101 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {TouchableOpacity, StyleSheet, Text, View, Image} from 'react-native';
 
 function PlayScreen({navigation}) {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text onPress={() => navigation.goBack()}>Settings!</Text>
+    <View style={styles.container}>
+      <View>
+        <View style={styles.back1}>
+          <View style={styles.back2}>
+            <Image
+              style={styles.img}
+              source={require('../../assets/image/poster.jpeg')}
+            />
+          </View>
+        </View>
+        <Text style={styles.textBold}>Dora Nalia</Text>
+        <Text style={styles.textLight}>Maluma</Text>
+      </View>
+
+      <View style={{flexDirection: 'row', backgroundColor: 'red'}}>
+        <TouchableOpacity>
+          <Image
+            style={{height: 30, width: 30}}
+            source={require('../../assets/image/arrowLeft.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            style={{height: 50, width: 50}}
+            source={require('../../assets/image/playy.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            style={{height: 30, width: 30}}
+            source={require('../../assets/image/arrowRight.png')}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 export default PlayScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#231E4B',
+    justifyContent: 'space-between',
+  },
+
+  back1: {
+    borderTopRightRadius: 200,
+    borderTopLeftRadius: 200,
+    borderBottomRightRadius: 200,
+    borderBottomLeftRadius: 200,
+    width: 280,
+    height: 280,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#8562F5',
+    marginTop: 50,
+  },
+  back2: {
+    borderTopRightRadius: 200,
+    borderTopLeftRadius: 200,
+    borderBottomRightRadius: 200,
+    borderBottomLeftRadius: 200,
+    width: 270,
+    height: 270,
+    backgroundColor: '#282357',
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  img: {
+    borderTopRightRadius: 200,
+    borderTopLeftRadius: 200,
+    borderBottomRightRadius: 200,
+    borderBottomLeftRadius: 200,
+    width: 245,
+    height: 240,
+    backgroundColor: 'black',
+    alignSelf: 'center',
+  },
+  textBold: {
+    marginHorizontal: 10,
+    color: 'white',
+    fontSize: 22,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    marginTop: 22,
+  },
+  textLight: {
+    marginHorizontal: 10,
+    color: 'white',
+    fontSize: 14,
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+});
